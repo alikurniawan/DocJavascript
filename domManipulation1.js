28bca35bf54fb2742a97f2621a4dd3b27ff1ac9a
alikurniawan <alikoerniawan02@gmail.com>
2018-07-18 09:03:14 +0700

    newLib

--- domManipulation1.js
+++ domManipulation1.js
@@ -8,8 +8,8 @@ a.setAttribute('class', 'satu'); //Nambahin Attribute di Element
 a.getAttribute('href'); //tampilin attribute elemen
 a.removeAttribute('href'); //Buang
 
-c.classList.add();
-c.classList.remove();
-c.classList.toggle();
-c.classList.item();
-c.classList.contain();
+c.classList.add('label'); //Nambahin class Label didalam class <p class="satu lael" >
+c.classList.remove('label');
+c.classList.toggle('label'); //Kalau udah ada kelas label dibiarin, kalau belum ditambahain
+c.classList.item(2); //Mencari urutan nomer 2 name class didalam class
+c.classList.contains('dua'); //adakah class name dua ? #Nanya dia jawabnya true or false
