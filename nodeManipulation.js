b3c7292c40459babff2c0ea6f9896ad4d221feec
alikurniawan <alikoerniawan02@gmail.com>
2018-07-18 10:03:22 +0700

    NodeManipulation

--- nodeManipulation.js
+++ nodeManipulation.js
@@ -1,8 +1,12 @@
-
-
 document.createElement();
 document.createNode();
 node.appendChild();
 node.insertBefore();
 parentNode.removeChild();
 parentNode.replaceChild();
+
+//buat ini --- > <p>Paragraf Baru</p>
+const pBaru = document.createElement('p'); //membuat Element baru
+const textBaru = document.createTextNode('Paragraf Baru'); //Bikin baru Text Paragraf
+
+pBaru.appendChild(textBaru); //Masukkin 'Paragraf Baru ke elemen p'
