b70592b75083483c09445c5d76a3260a4a8b1065
alikurniawan <alikoerniawan02@gmail.com>
2018-07-18 10:45:56 +0700

    NodeManipulation

--- nodeManipulation.js
+++ nodeManipulation.js
@@ -21,4 +21,8 @@ liBaru.appendChild(teksBaru); //Masukkan text ke dalam elemen li
 
 const ul = document.querySelector('section#b ul'); //Pilih ul di dalam section class b
 const li2 = ul.querySelector('li:nth-child(2)'); //Pilih Lokasi di elemen li nomer 2
+
+//Untuk const li2 bisa juga dengan cara :
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+
 ul.insertBefore(liBaru, li2); //Memasukkan save --> elemen liBaru dengan lokasi sebelum li2
