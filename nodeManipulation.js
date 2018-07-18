8f7f642f2d245aa07db3d2c446af47dbe6f1cc0b
alikurniawan <alikoerniawan02@gmail.com>
2018-07-18 10:08:20 +0700

    NodeManipulation

--- nodeManipulation.js
+++ nodeManipulation.js
@@ -10,3 +10,7 @@ const pBaru = document.createElement('p'); //membuat Element baru
 const textBaru = document.createTextNode('Paragraf Baru'); //Bikin baru Text Paragraf
 
 pBaru.appendChild(textBaru); //Masukkin 'Paragraf Baru ke elemen p'
+
+//Simpan dan tampilkan hasilnya ke web
+const sectionA = document.getElementById('a'); //Tangkap Element ber id a
+sectionA.appendChild(pBaru); //Masukkan
