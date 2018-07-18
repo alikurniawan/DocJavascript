2384f52b6c177b42632c9e8ca23f79b1cea7f05f
alikurniawan <alikoerniawan02@gmail.com>
2018-07-18 10:40:33 +0700

    NodeManipulation

--- nodeManipulation.js
+++ nodeManipulation.js
@@ -8,9 +8,17 @@ parentNode.replaceChild();
 //buat ini --- > <p>Paragraf Baru</p>
 const pBaru = document.createElement('p'); //membuat Element baru
 const textBaru = document.createTextNode('Paragraf Baru'); //Bikin baru Text Paragraf
-
-pBaru.appendChild(textBaru); //Masukkin 'Paragraf Baru ke elemen p'
+pBaru.appendChild(textBaru); //Masukkin 'Paragraf Baru ke elemen p' jadi <p>Paragraf Baru</p>
 
 //Simpan dan tampilkan hasilnya ke web
 const sectionA = document.getElementById('a'); //Tangkap Element ber id a
-sectionA.appendChild(pBaru); //Masukkan
+sectionA.appendChild(pBaru); //Masukkan ke akhir dari element
+
+//-----------------------------------------------------------------------
+const liBaru = document.createElement('li'); //Buat elemen li dulu
+const teksBaru = document.createTextNode('Item Baru'); //buat text nya
+liBaru.appendChild(teksBaru); //Masukkan text ke dalam elemen li
+
+const ul = document.querySelector('section#b ul'); //Pilih ul di dalam section class b
+const li2 = ul.querySelector('li:nth-child(2)'); //Pilih Lokasi di elemen li nomer 2
+ul.insertBefore(liBaru, li2); //Memasukkan save --> elemen liBaru dengan lokasi sebelum li2
