eece107cc168b9c822730297e4a24c662b1d96e7
alikurniawan <alikoerniawan02@gmail.com>
2018-07-19 11:38:58 +0700

    Math Random

--- js/math.js
+++ js/math.js
@@ -1,4 +1,8 @@
 //Menghasilkan Nilai acak Math antara 0 sd 1
 const x = Math.random();
+const y = Math.round();
+const z = Math.floor(); //Pemulatan kebawah
+const ab = Math.ceil(); //Langit2 --> Pembulatan ke Atas
+
 
 console.log(x);
