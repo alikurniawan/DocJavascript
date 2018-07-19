89351fdee02a69e98fc184b1c2fe4d8456fdfce3
alikurniawan <alikoerniawan02@gmail.com>
2018-07-19 12:29:27 +0700

    How to call input type slider

--- js/inputTypeSlider.js
+++ js/inputTypeSlider.js
@@ -1,6 +1,27 @@
-//Cara manggil <input type="slider" name="sMerah"/>
+//Cara manggil <input type="slider" name="sMerah" min="1" max="255"/>
+//<input type="slider" name="sMerah" min="1" max="255"/>
+//<input type="slider" name="sHijau" min="1" max="255"/>
+//<input type="slider" name="sBiru" min="1" max="255"/>
+
 const sMerah = document.querySelector('input[name=sMerah]');
+const sMerah = document.querySelector('input[name=sHijau]');
+const sMerah = document.querySelector('input[name=sBiru]');
 
 sMerah.addEventListener('slider', function(){
-  //Do Something...
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  document.body.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
+});
+sBiru.addEventListener('slider', function(){
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  document.body.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
+});
+sHijau.addEventListener('slider', function(){
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  document.body.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
 });
