bd588403da2bb26dda64525ed39cc2f7f99aaa24
alikurniawan <alikoerniawan02@gmail.com>
2018-08-01 14:32:02 +0700

    New Tut

---
+++ js/timeOut.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+
+container.addEventListener('click', function(){
+    if(e.target.className == 'thumb'){
+        jumbo.src = e.target.src;  //src menimpa sumber Image yg ada
+        jumbo.classList.add('fade');
+
+        setTimeout(function(){
+            jumbo.classList.remove('fade'); //fade adalah nama kelas di CSS
+        }, 500);
+
+        e.target.classList.add('active'); //Active adalah nama class di CSS
+
+    }
+})
