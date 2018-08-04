ab2c464c4ae702dc96828447c8046aec1fcf103c
alikurniawan <alikoerniawan02@gmail.com>
2018-08-05 03:43:15 +0700

    EditedBuildInCharacterJS

--- JS/buildinCharacter.js
+++ JS/buildinCharacter.js
@@ -1,8 +1,10 @@
+//Reference Doc https://www.w3schools.com/jsref/jsref_obj_math.asp
+
 var a = 'Ali Kurniawan';
 var b = 30;
 
-console.log(a.charAt(2));
-console.log(a.slice(4));
+console.log(a.charAt(2)); //result --> start dari 0 -- from var a --> i
+console.log(a.slice(4)); //memotong karakter start dari 1 bukan 0 -- result --> Kurniawan
 console.log(a.split());
 console.log(a.toString());
 console.log(a.toLowerCase());
---
+++ JS/for3.js
@@ -0,0 +1,6 @@
+var a = 'asu baget'
+
+console.log(a.charAt(2));
+console.log(a.slice(6));
+
+
