198a6cd9a9d00c439a077d4197d98121d0f8c55d
alikurniawan <alikoerniawan02@gmail.com>
2018-08-19 20:10:18 +0700

    PerulanganMAP

---
+++ JS/arrayMAP.js
@@ -0,0 +1,8 @@
+//Perulangan
+var angka = [1,2,3,4,5,6,7,8];
+
+var sd = angka.map(function(e){
+    return e;
+})
+
+console.log(sd.join('-'))
---
+++ JS/sort.js
@@ -0,0 +1,5 @@
+var angka = [5,2,1,3,4]
+
+angka.sort()
+console.log(angka)
+//result --> 1,2,3,4,5 -->berbentuk array
